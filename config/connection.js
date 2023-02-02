a02f791526833a8c69d412aac0326157a42e72cd
MrutyunjayaRao <mjr.mca@gmail.com>
2023-02-02 17:47:57 +0530

    Conn updated

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-var connection = mysql.createConnection({
+// Initialize pool
+var connection      =    mysql.createPool({
+    connectionLimit : 10,
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-    if(!err){
-        console.log("Database is connected successfully!");
-    } else {
-        console.log(err);
-    }
-});
-
+    database: process.env.DB_NAME,
+    debug    :  false
+});    
 module.exports = connection;
