aa2478a03635e43034c124f6bdb60e4751f01877
MrutyunjayaRao <mjr.mca@gmail.com>
2023-03-27 12:35:58 +0530

    Get Sch Time Lines Added
    Get Sch Time Lines Added

--- routes/schRoutes.js
+++ routes/schRoutes.js
@@ -5,12 +5,14 @@ const{
     getSchedules,
     createSchedule,
     updateSchedule,
-    getSchedulesByEmpId
+    getSchedulesByEmpId,
+    getTimelines
 } = require('../controllers/schedule');
 
 router.get('/getSchedules', getSchedules);
 router.post('/createSchedule', createSchedule);
 router.patch('/updateSchedule/:id', updateSchedule);
 router.get('/getSchedulesByEmpId/:id', getSchedulesByEmpId);
+router.get('/getTimelines', getTimelines);
 
 module.exports = router ;
